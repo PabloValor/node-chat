6edc7bff70994c4c7ab87163572220cf7fae43c6
Pablo Valor <pablovalor89@gmail.com>
2014-11-08 15:47:31 -0300

    Config server.js

--- server.js
+++ server.js
@@ -1,2 +1,19 @@
-var	express = require('express'),
-	app = express();
+var express = require('express'),
+      socket = require('socket.io');
+
+app = express();
+
+
+var port = process.env.PORT || 8000;
+
+app.use('/public', express.static(__dirname + '/public')); // assets (css/js/img)
+
+
+app.get('/', function (req, res) {
+      res.sendFile(__dirname + '/views/index.html');
+});
+
+app.listen(port, function() {
+
+      console.log('App running on port: ' + port);
+});

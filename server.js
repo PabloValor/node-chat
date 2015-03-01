975e94828a9fe2d9075394fba7f3458ad2525e66
Pablo Valor <pablovalor89@gmail.com>
2015-03-01 04:04:02 -0300

    setup firebase and socket.io.session

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
-var http		= require('http'),
-	express 	= require('express'),
-	app 		= express(),
-	server 		= http.createServer(app),
-	bodyParser 	= require('body-parser'),
-	io 			= require('socket.io')(server),
-	swig 		= require('swig'),
-	session 	= require('express-session'),
-	cookieParser= require('cookie-parser'),
-	favicon 	= require('serve-favicon');
+var http			= require('http'),
+	express 		= require('express'),
+	app 			= express(),
+	server 			= http.createServer(app),
+	bodyParser 		= require('body-parser'),
+	io 				= require('socket.io')(server),
+	swig 			= require('swig'),
+	session 		= require('express-session'),
+	FirebaseStore 	= require('connect-firebase')(session),
+	socketIOSession	= require('socket.io.session'),
+	cookieParser 	= require('cookie-parser'),
+	favicon 		= require('serve-favicon');
 
 var port = process.env.PORT || 3030;
 
@@ -22,17 +24,39 @@ app.set('views', __dirname + '/app/public/views/');
 //set static files
 app.use(express.static(__dirname + '/app/public/assets/'));
 
-//Models?
+//Current users collection
 var users = [];
-var newUserName;
 
 //set favicon
 app.use(favicon(__dirname + '/favicon.ico'));
 
 //middlewares
 app.use(cookieParser());
-app.use(session({secret: '123secret'}));
+
+/*  Prepare Session */
+var options = {
+
+  // The Firebase URL 
+  host: 'chat-zen.firebaseio.com',
+
+  // Optional. How often expired sessions should be cleaned up.
+  // 86400000 miliseconds (24 hours).
+  reapInterval: 86400000
+
+  // Optional. A Firebase authentication token
+  //token: 'qKtOKAQSTCxLFJI7uSeof6H7cfLpSuWYOhqOTQqz'
+};
+
+var sessionStore = new FirebaseStore(options);
+
+var sessionSettings = {
+	store: sessionStore, 
+  	secret: 'keyboard cat' 
+};
+app.use(session(sessionSettings));
+
 app.use(bodyParser.urlencoded({extended: true}));
+
 
 var isnotLogged = function(req, res, next) {
 	if(!req.session.username) {
@@ -57,29 +81,36 @@ app.get('/login', isLogged, function(req, res){
 
 app.post('/login', function(req, res){
 	users.push(req.body.username);
-	req.session.username = req.body.username; newUserName = req.body.username;
+	req.session.username = req.body.username;
 	res.redirect('/');
 });
 
 app.get('/', isnotLogged , function(req, res){
+	users.push(req.body.username);
 	res.render('chat',{});
 });
 
 // Socket.io events listener
 
+//socket.session == req.session, change one of them, the other one change too
+var socketSession = socketIOSession(sessionSettings);
+io.use(socketSession.parser);
+
 io.on('connection', function(socket){
-	socket.nickname = newUserName;
+
+	io.sockets.emit('users counter', {usersCount: users.length});
 
 	socket.on('chat message', function(msg){
-		io.emit('chat message', {msg: msg, username: socket.nickname});
+		io.emit('chat message', {msg: msg, username: socket.session.username});
 	});
 
 	io.sockets.emit('users counter', {usersCount: users.length});
-	io.sockets.emit('show newUser', {username: newUserName});
+	io.sockets.emit('show newUser', {username: socket.session.username});
 
 	socket.on('disconnect', function(){
-		removeInArray(users, socket.nickname);
-		io.sockets.emit('user offline',{username: socket.nickname});
+		removeInArray(users, socket.session.username);
+
+		io.sockets.emit('user offline',{username: socket.session.username});
 		io.sockets.emit('users counter', {usersCount: users.length});
 	});		
 });
